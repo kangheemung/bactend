cc11352a6ae9d6f7aae2dadaa137b633324f6078
EC2 Default User <ec2-user@ip-10-0-0-211.ap-northeast-1.compute.internal>
2023-07-29 14:19:32 +0000

    post Earr

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,6 +26,6 @@ end
 private
 
     def session_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:name, :email, :password)
     end
 end
